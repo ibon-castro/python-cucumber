d650d1cad202442689c7edf7fb5c968dbb2eb09b
ibon-castro <69823509+ibon-castro@users.noreply.github.com>
2025-02-17 20:43:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def reset():
     return render_template_string(HTML_TEMPLATE, count=count)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
